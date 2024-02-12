8ab22df960da1aa35c441c337ed3aa274b8bc426
Beratungen und Digitale Dienstleistungen <52482809+calyaconsult@users.noreply.github.com>
2024-02-12 07:46:14 +0100

    Update bash-script.sh

--- resources/bash-script.sh
+++ resources/bash-script.sh
@@ -4,6 +4,7 @@
 # Test mit einem String; das Zeilenende wird mit `_` markiert
 echo "A,B,C,D_E,F,G,H_I,J,K,L_M,N,O,P_Q,R,S,T_U,V,X,Y" | tr _ '\n' | sed 's/,/<\/td><td>/g' | sed 's/^\(.*\)$/<tr><td>\1<\/td><\/tr>/' | sed '1 s/^/<table>\n/'; echo '</table>'
 
+# Beispiel 2
 # Ganzes File lesen
 # Immer daran denken, dass sich am Beginn der Datei ein BOM und am Zeilenende ein '\r' verstecken könnte! | sed '1 s/\xEF\xBB\xBF//'
 
